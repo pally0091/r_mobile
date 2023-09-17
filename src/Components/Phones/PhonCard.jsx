4a37db23d55bd2f3ff4a61f45510719ca6fdecf2
Debashis Roy <goodquote39@hotmail.com>
2023-09-17 14:18:35 +0600

    cards hover effects fixed

--- src/Components/Phones/PhonCard.jsx
+++ src/Components/Phones/PhonCard.jsx
@@ -6,18 +6,18 @@ import { Link } from "react-router-dom";
 const PhonCard = ({ phone }) => {
   const { brand, phone_name, slug, image } = phone;
   return (
-    <div className="h-64 rounded-xl relative  hover:-mt-2 transition-all duration-500">
+    <div className="h-64 rounded-xl relative  transition-all duration-500 overflow-hidden card">
       <img
         className="w-full h-full rounded-xl"
         src={image}
         alt={phone_name}
       />
 
-      <div className="absolute bottom-0 text-center w-full bg-black bg-opacity-60">
+      <div className="absolute bottom-full text-center w-full bg-black bg-opacity-60 card-info ">
         <p>{brand}</p>
         <p className="text-xl font-semibold">{phone_name}</p>
         <Link to={`/${slug}`}>
-          <button className="bg-blue-300 text-black w-full py-2 rounded-b-xl hover:bg-blue-800 hover:text-white transition-all duration-500">
+          <button className="bg-blue-300 text-black w-full py-2  hover:bg-blue-800 hover:text-white transition-all duration-500">
             Details
           </button>
         </Link>
